91c1d0e131aad97c4ad2fee3ef43332384fb290c
loxigl <43906792+loxigl@users.noreply.github.com>
2021-09-10 14:12:48 +0300

    пофиксил все ошибки на которые ругалась студия

--- app/src/main/java/com/example/project/LoginScreen.java
+++ app/src/main/java/com/example/project/LoginScreen.java
@@ -2,21 +2,18 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
+
 import android.widget.Toast;
 
 
 public class LoginScreen extends AppCompatActivity {
-    private Button btn;
-    private SharedPreferences name;
-    private SharedPreferences age;
-    private SharedPreferences gender;
     private String str1;
     private String str2;
     public static final String MY_PREFS_NAME="login";
@@ -27,20 +24,18 @@ public class LoginScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        btn=(Button) findViewById(R.id.button);
-        SharedPreferences prefs = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE);
+        Button btn = findViewById(R.id.button);
+       // SharedPreferences prefs = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE);
 
 
 
 
         btn.setOnClickListener(view -> {
             Intent intent = new Intent(LoginScreen.this, MainScreen.class);
-            EditText s1= (EditText) findViewById(R.id.name);
-            EditText s2= (EditText) findViewById(R.id.age) ;
+            EditText s1= findViewById(R.id.name);
+            EditText s2= findViewById(R.id.age);
             str1=s1.getText().toString();
             str2=s2.getText().toString();
-            /*intent.putExtra("Name",str1);
-            intent.putExtra("Age",str2);*/
             startActivity(intent);
             SharedPreferences.Editor editor = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE).edit();
             editor.putBoolean("userRegistered", true);
@@ -51,7 +46,8 @@ public class LoginScreen extends AppCompatActivity {
 
         });
     }
-    ImageButton button;
+
+   @SuppressLint("NonConstantResourceId")
    public void onClick(View view)
    {
        SharedPreferences.Editor editor = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE).edit();
