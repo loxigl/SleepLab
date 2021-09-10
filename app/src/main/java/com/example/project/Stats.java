7149d640ab3974df49d60eedd005df243b5cee00
loxigl <43906792+loxigl@users.noreply.github.com>
2021-09-10 14:29:17 +0300

    пасхалка иксди

--- app/src/main/java/com/example/project/Stats.java
+++ app/src/main/java/com/example/project/Stats.java
@@ -2,7 +2,9 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Stats extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class Stats extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stats);
+        Button btn=findViewById(R.id.pasha);
+        btn.setOnClickListener(view -> {
+
+            Intent intent=new Intent(Stats.this,Five.class);
+            startActivity(intent);
+        });
     }
 }
