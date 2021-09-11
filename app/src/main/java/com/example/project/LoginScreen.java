72e4635c1c16e01747227e7974a059683dcb398f
loxigl <43906792+loxigl@users.noreply.github.com>
2021-09-11 03:20:43 +0300

    дофига работы

--- app/src/main/java/com/example/project/LoginScreen.java
+++ app/src/main/java/com/example/project/LoginScreen.java
@@ -17,6 +17,7 @@ public class LoginScreen extends AppCompatActivity {
     private String str1;
     private String str2;
     public static final String MY_PREFS_NAME="login";
+    private Boolean test=false;
 
 
 
@@ -36,12 +37,22 @@ public class LoginScreen extends AppCompatActivity {
             EditText s2= findViewById(R.id.age);
             str1=s1.getText().toString();
             str2=s2.getText().toString();
-            startActivity(intent);
-            SharedPreferences.Editor editor = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE).edit();
-            editor.putBoolean("userRegistered", true);
-            editor.putString("name",str1);
-            editor.putString("age",str2);
-            editor.apply();
+
+
+            if (str1.length()!=0 & str2.length()!=0 & test) {
+                SharedPreferences.Editor editor = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE).edit();
+                editor.putBoolean("userRegistered", true);
+                editor.putString("name",str1);
+                editor.putString("age",str2);
+                editor.apply();
+                startActivity(intent);
+            }
+            else
+            {
+                Toast.makeText(this, "Заполни поля, имей совесть", Toast.LENGTH_SHORT).show();
+            }
+
+
 
 
         });
@@ -53,11 +64,12 @@ public class LoginScreen extends AppCompatActivity {
        SharedPreferences.Editor editor = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE).edit();
        switch (view.getId()) {
            case R.id.men:
-
+               test=true;
                editor.putBoolean("man",true);
                editor.apply();
                break;
            case R.id.women:
+               test=true;
                editor.putBoolean("man",false);
                editor.apply();
                break;
--- app/src/main/java/com/example/project/Settings.java
+++ app/src/main/java/com/example/project/Settings.java
@@ -2,13 +2,34 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Settings extends AppCompatActivity {
+
+    private static final String MY_PREFS_NAME ="login" ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-    }
+        Button btn = findViewById(R.id.reset);
+        // SharedPreferences prefs = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE);
+
+
+
+
+        btn.setOnClickListener(view -> {
+            Intent intent = new Intent(Settings.this, LoginScreen.class);
+            SharedPreferences.Editor editor = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE).edit().clear();
+            editor.clear();
+            editor.apply();
+            startActivity(intent);
+
+
+    });
+
+}
 }
