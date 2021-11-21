76bf31450d2c67457e5b452e620f5a9f513c24a6
volko <volkov.volkova2000@mail.ru>
2021-11-21 16:44:02 +0300

    Пытаюсь сделать опросник

---
+++ app/src/main/java/com/example/project/Array.java
@@ -0,0 +1,21 @@
+package com.example.project;
+
+import android.hardware.biometrics.BiometricManager;
+
+public class Array {
+
+    final int[] points = {
+
+    };
+
+    final int[] probability = {
+            R.string.q1,
+            R.string.q2,
+            R.string.q3,
+            R.string.q4,
+            R.string.q5,
+            R.string.q6,
+            R.string.q7,
+            R.string.q8,
+    };
+}
--- app/src/main/java/com/example/project/QaA.java
+++ app/src/main/java/com/example/project/QaA.java
@@ -2,13 +2,35 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class QaA extends AppCompatActivity {
 
+    public int que;
+    Array array = new Array();
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qa);
+
+        Button back_to_maskot = (Button)findViewById(R.id.back_to_maskot);
+        back_to_maskot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Intent intent = new Intent(QaA.this,Mascot.class);
+                    startActivity(intent);
+                    finish();
+                }catch (Exception e) {
+
+                }
+            }
+        });
     }
 }
